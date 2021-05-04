b69fb1ba02a8a7737f86834f776601cd006fca70
e-oncologia <52789797+e-oncologia@users.noreply.github.com>
2021-05-04 10:12:57 +0200

    nmx

--- ES/M3/assets/js/main.js
+++ ES/M3/assets/js/main.js
@@ -467,4 +467,64 @@ function muestra1() {
 	}
   }
 
-  
+// FLIP CARD
+var card1 = document.querySelector('.card1');
+card1.addEventListener( 'click', function() {
+  card1.classList.toggle('is-flipped');
+});
+
+var card2 = document.querySelector('.card2');
+card2.addEventListener( 'click', function() {
+  card2.classList.toggle('is-flipped');
+});
+
+var card3 = document.querySelector('.card3');
+card3.addEventListener( 'click', function() {
+  card3.classList.toggle('is-flipped');
+});
+
+var card4 = document.querySelector('.card4');
+card4.addEventListener( 'click', function() {
+  card4.classList.toggle('is-flipped');
+});
+
+var card5 = document.querySelector('.card5');
+card5.addEventListener( 'click', function() {
+  card5.classList.toggle('is-flipped');
+});
+
+var card6 = document.querySelector('.card6');
+card6.addEventListener( 'click', function() {
+  card6.classList.toggle('is-flipped');
+});
+
+var card7 = document.querySelector('.card7');
+card7.addEventListener( 'click', function() {
+  card7.classList.toggle('is-flipped');
+});
+
+var card8 = document.querySelector('.card8');
+card8.addEventListener( 'click', function() {
+  card8.classList.toggle('is-flipped');
+});
+
+var card9 = document.querySelector('.card9');
+card9.addEventListener( 'click', function() {
+  card9.classList.toggle('is-flipped');
+});
+
+var card10 = document.querySelector('.card10');
+card10.addEventListener( 'click', function() {
+  card10.classList.toggle('is-flipped');
+});
+
+var card11 = document.querySelector('.card11');
+card11.addEventListener( 'click', function() {
+  card11.classList.toggle('is-flipped');
+});
+
+var card12 = document.querySelector('.card12');
+card12.addEventListener( 'click', function() {
+  card12.classList.toggle('is-flipped');
+});
+
