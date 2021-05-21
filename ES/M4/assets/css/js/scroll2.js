4e1dbdaf1d8a4774a09f1434af39322dcd3ea5fb
e-oncologia <52789797+e-oncologia@users.noreply.github.com>
2021-05-21 13:22:58 +0200

    Delete scroll2.js

--- ES/M4/assets/css/js/scroll2.js
+++
@@ -1,9 +0,0 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction1()};
-
-function myFunction1() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 75;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
